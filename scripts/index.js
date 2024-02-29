975dfae6afa33363e4a189da7cbcf3227041195d
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-02-29 23:55:36 +0200

    js

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,11 @@
 const btn=document.getElementById('btn');
-const userInput=document.getElementById('username').value;
-const passInput=document.getElementById('password').value;
-
-const credentials={
-    username: 'AdminSEF123',
-    password: 'SeF@ctORy$$456',
-};
 
 btn.addEventListener("click",function(){
-    if (userInput === credentials.username && passInput === credentials.password) {
-        window.location.href = 'todo.html';
+    const userInput=document.getElementById('username').value;
+    const passInput=document.getElementById('password').value;
+
+    if (userInput === "AdminSEF123" && passInput === "SeF@ctORy$$456") {
+        window.location.href = "todo.html";
     }
     else{
         alert('Invalid username or password');

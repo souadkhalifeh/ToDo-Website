d5045adeded58a0cde168b97d6c2f4bcd63eebb4
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-03-01 07:35:37 +0200

    todo js

---
+++ todo.js
@@ -0,0 +1,9 @@
+const todo=document.getElementById('todo');
+const addButton=document.getElementById('addTodo');
+const todoList=document.getElementById('todoList');
+
+let todos=[];
+
+function addTodo(){
+
+}

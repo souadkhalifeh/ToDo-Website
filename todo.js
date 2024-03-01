38fff4071cb0646306a4d7ee78253684483cd053
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-03-01 09:31:41 +0200

    edited

--- todo.js
+++ todo.js
@@ -1,9 +1,50 @@
-const todo=document.getElementById('todo');
-const addButton=document.getElementById('addTodo');
-const todoList=document.getElementById('todoList');
+const input = document.getElementById('todo');
+const addBtn = document.getElementById('addBtn');
+const list = document.querySelector('.todolist'); // Changed document.get to document.querySelector
 
-let todos=[];
-
-function addTodo(){
-
+function clickButton(e) {
+    e.preventDefault();
+    addTodo();
 }
+
+function addTodo() {
+    const itemall = document.createElement('div');
+    itemall.classList.add('itemall');
+
+    const item = document.createElement('p');
+    item.classList.add('item');
+    item.innerText = input.value;
+    itemall.appendChild(item);
+
+    if (input.value === '') {
+        return;
+    }
+    const checkbutton = document.createElement("button");
+    checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkbutton.classList.add("check-button");
+    itemall.appendChild(checkbutton);
+
+    const trashbutton = document.createElement("button");
+    trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    trashbutton.classList.add("trash-button");
+    itemall.appendChild(trashbutton);
+
+    list.appendChild(itemall);
+    input.value = '';
+}
+
+function deleteToDo(e) {
+    const item = e.target;
+    if (item.classList.contains('check-button')) { 
+        const todolist = item.parentElement;
+        todolist.classList.toggle('checklist');
+    }
+
+    if (item.classList.contains('trash-button')) { 
+        const todolist = item.parentElement;
+        todolist.remove();
+    }
+}
+
+addBtn.addEventListener('click', clickButton);
+list.addEventListener('click', deleteToDo);
